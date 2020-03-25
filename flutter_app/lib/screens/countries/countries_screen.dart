9a3aa22e9482e76b2e46862f1091278305abde78
Emre Köse <e.kose26@gmail.com>
2020-03-25 14:25:23 +0300

    initial commit

---
+++ flutter_app/lib/screens/countries/countries_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Countries extends StatefulWidget {
+  static const routeName = '/countries';
+  @override
+  _CountriesState createState() => _CountriesState();
+}
+
+class _CountriesState extends State<Countries> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
